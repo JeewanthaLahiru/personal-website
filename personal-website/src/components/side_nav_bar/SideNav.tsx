900f360a8f38623bc16a8bf1cea3bf56b969c386
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-03-20 14:24:54 +0530

    added hover effect to navbar

--- personal-website/src/components/side_nav_bar/SideNav.tsx
+++ personal-website/src/components/side_nav_bar/SideNav.tsx
@@ -2,29 +2,35 @@ import React from 'react';
 import {Button, Col, Form, FormControl, Nav, Navbar, NavDropdown, Row} from "react-bootstrap";
 
 const SideNav:React.FC = () => {
+
     return(
             <Col md={3} className="side-nav-bar m-0" >
                 <Navbar expand="md">
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Row className="side-nav-bar-item">
-                            <Col>
+                            <Col className="p-1">
                                 <ul className="navbar-nav mr-auto flex-column vertical-nav">
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#">Dashboard</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#">Pages</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#">Link</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#">Another Link</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#">Setting</a>
-                                    </li>
+                                    <Nav.Item>
+                                        <Nav.Link className="text-left">
+                                            Profile
+                                        </Nav.Link>
+                                    </Nav.Item>
+                                    <Nav.Item>
+                                        <Nav.Link className="text-left">
+                                            Education
+                                        </Nav.Link>
+                                    </Nav.Item>
+                                    <Nav.Item>
+                                        <Nav.Link className="text-left">
+                                            Portfolio
+                                        </Nav.Link>
+                                    </Nav.Item>
+                                    <Nav.Item>
+                                        <Nav.Link className="text-left">
+                                            Contact
+                                        </Nav.Link>
+                                    </Nav.Item>
                                 </ul>
                             </Col>
                         </Row>
