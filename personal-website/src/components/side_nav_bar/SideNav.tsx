d8d3c91631f87a059c5e07b2611663e6cc02ff6c
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-03-20 15:07:44 +0530

    brand name background image

--- personal-website/src/components/side_nav_bar/SideNav.tsx
+++ personal-website/src/components/side_nav_bar/SideNav.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import {Button, Col, Form, FormControl, Nav, Navbar, NavDropdown, Row} from "react-bootstrap";
+import {Button, Col, Form, FormControl, Image, Nav, Navbar, NavDropdown, Row} from "react-bootstrap";
+import Particles from '../../assets/images/spakly/particles.gif';
 
 const SideNav:React.FC = () => {
 
     return(
-            <Col md={3} className="side-nav-bar m-0" >
+            <Col md={2} className="side-nav-bar m-0" >
                 <Navbar expand="md">
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
+                        <Row className="p-0">
+                            <Col xs={12} className="brand-name-background" >
+                                <h1 className="text-white text-center mt-3 brand-name">Jeewantha</h1>
+                            </Col>
+                        </Row>
                         <Row className="side-nav-bar-item">
                             <Col className="p-1">
                                 <ul className="navbar-nav mr-auto flex-column vertical-nav">
