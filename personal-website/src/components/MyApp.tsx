3813da7fc4a207327ea5ae97f4dcddd4c3d19500
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-04-28 19:36:00 +0530

    created myapp component

---
+++ personal-website/src/components/MyApp.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Container} from "react-bootstrap";
+
+const MyApp:React.FC = () => {
+    return(
+        <Container className="m-0 my-app" >
+            hello my app
+        </Container>
+    )
+}
+
+export default MyApp;
