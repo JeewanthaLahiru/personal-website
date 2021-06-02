0b64ba24b70a36d6b97883ddf294780fb7c027ed
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-06-02 13:21:17 +0530

    added onclick to open social media

--- personal-website/src/components/home/SocialIcon.tsx
+++ personal-website/src/components/home/SocialIcon.tsx
@@ -6,10 +6,15 @@ type socialIconProps = {
     socialLink : string
 }
 
+const handleOnClick = (url:string) => {
+    const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+    if (newWindow) newWindow.opener = null
+}
+
 const SocialIcon:React.FC<socialIconProps> = (props) => {
     return(
         <div className="socialIcon col-md-15" >
-            <div className="innerDiv">
+            <div onClick={() => handleOnClick(props.socialLink)} className="innerDiv">
                 <Image src={props.imageLink} />
             </div>
 
