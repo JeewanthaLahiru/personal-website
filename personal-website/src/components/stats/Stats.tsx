bf927b0d3220fedbb2f7b4a06a23109299d8639f
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-06-16 15:06:12 +0530

    added social icons

--- personal-website/src/components/stats/Stats.tsx
+++ personal-website/src/components/stats/Stats.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
-import NotFound from "../notfound/NotFound";
+import Github from "../../assets/images/social media image/github.png";
+import Stack from "../../assets/images/social media image/stackoverflow.png";
+import Medium from "../../assets/images/social media image/medium.png";
+import {Col, Image, Row} from "react-bootstrap";
 
 const Stats:React.FC = () => {
     return(
         <React.Fragment>
-            <NotFound/>
+            <Row className="mx-0 pt-5">
+                <Col xs={6} className="pr-xl-0">
+                    <Row className="m-0 p-0">
+                        <Col xs={12} className="stat-item p-0" >
+                            <Row className="m-0">
+                                <Col xs={2}>
+                                    <Image src={Github} />
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Col>
+                <Col xs={6}>
+                    <Row className="mx-0">
+                        <Col xs={12} className="stat-item" >
+                            <Row className="m-0">
+                                <Col xs={2}>
+                                    <Image src={Medium} />
+                                </Col>
+                            </Row>
+                        </Col>
+                        <Col xs={12} className="stat-item" >
+                            <Row className="m-0">
+                                <Col xs={2}>
+                                    <Image src={Stack} />
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
         </React.Fragment>
     )
 }
